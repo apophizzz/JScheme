76429dfe1286e741f4e386ffa5fc9d751e1853ca
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-04-26 17:37:53 +0200

    Added more constants

--- src/main/java/hdm/pk070/jscheme/SchemeConstants.java
+++ src/main/java/hdm/pk070/jscheme/SchemeConstants.java
@@ -6,4 +6,10 @@ package hdm.pk070.jscheme;
 public class SchemeConstants {
 
     public static final int EOF = -1;
+
+    public static final String BOOL_TRUE_VAL = "#t";
+    public static final String BOOL_FALSE_VAL = "#f";
+    public static final String NIL_VAL = "nil";
+
+    public static final int INITIAL_SYMBOL_TABLE_SIZE = 511;
 }
