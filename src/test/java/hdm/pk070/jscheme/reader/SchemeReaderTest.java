776d7b0ab20f3e50d43b038729ec534ad6b43980
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-04-26 15:20:47 +0200

    Modified and added test cases corresponding to changes in SchemeReader class

--- src/test/java/hdm/pk070/jscheme/reader/SchemeReaderTest.java
+++ src/test/java/hdm/pk070/jscheme/reader/SchemeReaderTest.java
@@ -1,8 +1,7 @@
 package hdm.pk070.jscheme.reader;
 
 import hdm.pk070.jscheme.obj.SchemeObject;
-import hdm.pk070.jscheme.obj.type.SchemeInteger;
-import hdm.pk070.jscheme.obj.type.SchemeString;
+import hdm.pk070.jscheme.obj.type.*;
 import hdm.pk070.jscheme.util.ReflectionUtils;
 import org.junit.After;
 import org.junit.Test;
@@ -19,16 +18,20 @@ import static org.junit.Assert.*;
  */
 public class SchemeReaderTest {
 
+    private static final String METHOD_READ_NUMBER = "readNumber";
+    private static final String METHOD_READ_STRING = "readString";
+    private static final String METHOD_READ_SYMBOL = "readSymbol";
+
     private SchemeReader schemeReader;
 
 
     @Test
     public void testCreateSchemeReaderInstance() {
-        SchemeReader schemeReaderStdIn = SchemeReader.withStdin();
-        SchemeReader schemeReaderWithStream = SchemeReader.withInputStream(System.in);
+        schemeReader = SchemeReader.withStdin();
+        assertThat(schemeReader, notNullValue());
 
-        assertThat(schemeReaderStdIn, notNullValue());
-        assertThat(schemeReaderWithStream, notNullValue());
+        schemeReader = SchemeReader.withInputStream(System.in);
+        assertThat(schemeReader, notNullValue());
     }
 
     @Test(expected = NullPointerException.class)
@@ -40,8 +43,8 @@ public class SchemeReaderTest {
     @Test
     public void testReadNumber() {
         String testInput = "1234";
-        SchemeReader schemeReader = SchemeReader.withInputStream(new ByteArrayInputStream(testInput.getBytes()));
-        Object number = ReflectionUtils.invoke(schemeReader, "readNumber");
+        schemeReader = SchemeReader.withInputStream(new ByteArrayInputStream(testInput.getBytes()));
+        Object number = ReflectionUtils.invoke(schemeReader, METHOD_READ_NUMBER);
 
         assertThat("number is null!", number, notNullValue());
         assertThat("number is not of type SchemeObject!", SchemeObject.class.isAssignableFrom(number.getClass()),
@@ -54,9 +57,9 @@ public class SchemeReaderTest {
 
     @Test
     public void testReadString() {
-        String testInput = "This is just a test";
-        SchemeReader schemeReader = SchemeReader.withInputStream(new ByteArrayInputStream(testInput.getBytes()));
-        Object string = ReflectionUtils.invoke(schemeReader, "readString");
+        String testInput = "\"This is just a test\"";
+        schemeReader = SchemeReader.withInputStream(new ByteArrayInputStream(testInput.getBytes()));
+        Object string = ReflectionUtils.invoke(schemeReader, METHOD_READ_STRING);
 
         assertThat("string is null!", string, notNullValue());
         assertThat("string is not of type SchemeObject!", SchemeObject.class.isAssignableFrom(string.getClass()),
@@ -66,6 +69,29 @@ public class SchemeReaderTest {
         assertThat("string does not have expected value!", ((SchemeString) string).getValue(), equalTo(testInput));
     }
 
+
+    @Test
+    public void testReadSymbol() {
+        assertReadSymbol("nil", new SchemeNil());
+        assertReadSymbol("#t", new SchemeTrue());
+        assertReadSymbol("#f", new SchemeFalse());
+    }
+
+
+    private <T extends SchemeSymbol> void assertReadSymbol(String input, SchemeSymbol expectedSymbol) {
+        Objects.requireNonNull(expectedSymbol);
+        schemeReader = SchemeReader.withInputStream(new ByteArrayInputStream(input.getBytes()));
+        Object symbol = ReflectionUtils.invoke(schemeReader, METHOD_READ_SYMBOL);
+
+        assertThat("symbol is null!", symbol, notNullValue());
+        assertThat("symbol is not of type SchemeObject!", SchemeObject.class.isAssignableFrom(symbol.getClass()),
+                equalTo(true));
+        assertThat("symbol is not of type SchemeSymbol!", SchemeSymbol.class.isAssignableFrom(symbol.getClass()),
+                equalTo(true));
+        assertThat(String.format("symbol is not of type %s!", expectedSymbol.getClass().getSimpleName()), (
+                (SchemeSymbol) symbol).typeOf(expectedSymbol.getClass()), equalTo(true));
+        assertThat("symbol does not have expected value!", symbol, equalTo(expectedSymbol));
+    }
 
     @After
     public void tearDown() {
