cf4daf304c9642c9c6e2f63778b7237b6c65f8ca
Patrick Kleindienst <patrick.kleindienst@web.de>
2016-04-27 15:14:32 +0200

    Implemented getOrAdd functionality

--- src/main/java/hdm/pk070/jscheme/symbolTable/SchemeSymbolTable.java
+++ src/main/java/hdm/pk070/jscheme/symbolTable/SchemeSymbolTable.java
@@ -1,6 +1,7 @@
 package hdm.pk070.jscheme.symbolTable;
 
 import hdm.pk070.jscheme.SchemeConstants;
+import hdm.pk070.jscheme.error.SchemeError;
 import hdm.pk070.jscheme.hash.HashAlgProvider;
 import hdm.pk070.jscheme.hash.impl.StandardHashAlgProvider;
 import hdm.pk070.jscheme.obj.type.SchemeSymbol;
@@ -13,8 +14,10 @@ import java.util.Objects;
 public class SchemeSymbolTable {
 
     private static int tableSize = SchemeConstants.INITIAL_SYMBOL_TABLE_SIZE;
-    private static SchemeSymbolTable symbolTable;
+    private static SchemeSymbolTable schemeSymbolTable;
 
+    private SchemeSymbol[] symbolTable;
+    private int tableFillSize;
     private HashAlgProvider hashAlgProvider;
 
     public static SchemeSymbolTable getInstance() {
@@ -22,27 +25,84 @@ public class SchemeSymbolTable {
     }
 
     public static SchemeSymbolTable withHashAlgorithm(HashAlgProvider hashAlgProvider) {
-        if (Objects.isNull(symbolTable)) {
-            symbolTable = new SchemeSymbolTable(new StandardHashAlgProvider());
+        if (Objects.isNull(schemeSymbolTable)) {
+            schemeSymbolTable = new SchemeSymbolTable(hashAlgProvider);
         }
-        return symbolTable;
+        return schemeSymbolTable;
     }
 
     private SchemeSymbolTable(HashAlgProvider hashAlgProvider) {
         this.hashAlgProvider = hashAlgProvider;
+        this.symbolTable = new SchemeSymbol[tableSize];
+        this.tableFillSize = 0;
     }
 
-    // TODO: Manage global symbols
-    // If symbol table already contains object, return the corresponding SchemeSymbol object
-    // If symbol table does not contain the object, construct SchemeSymbol object, store it and return it
+
     public SchemeSymbol getOrAdd(String symbolName) {
         Objects.requireNonNull(symbolName);
 
-        // compute hash value from symbol name
+        int nextIndex;
         int hashVal = hashAlgProvider.computeHash(symbolName);
+        int startIndex = hashVal % tableSize;
+        SchemeSymbol symbol;
+        nextIndex = startIndex;
 
-        // TODO: symbol table lookup
-        return null;
+        for (; ; ) {
+
+            // In case the current index marks a free slot
+            if (isFreeSlot(nextIndex)) {
+                SchemeSymbol schemeSymbol = SchemeSymbol.createObj(symbolName);
+                addToTable(schemeSymbol, nextIndex);
+                incrementFillSize();
+                doRehashIfRequired();
+
+                return schemeSymbol;
+            }
+
+            // in case the slot is occupied: check if object in slot is searched symbol
+            symbol = symbolTable[nextIndex];
+            if (symbol.getValue().equals(symbolName)) {
+                // return symbol in slot 'nextIndex'
+                return symbol;
+            }
+
+            // if the symbol at slot 'nextIndex' is not the symbol we searched for, we have
+            // a hash collision > store new symbol at next slot available
+            nextIndex = ++nextIndex % tableSize;
+
+            // if there's no free slot available in the table, throw error
+            if (nextIndex == startIndex) {
+                SchemeError.print("Symbol table problem!");
+            }
+        }
+    }
+
+    private void doRehashIfRequired() {
+        if (tableFillSize > 0.75 * tableSize) {
+            startRehash();
+        }
+    }
+
+    private void startRehash() {
+        // TODO implement rehash
+    }
+
+
+    private void incrementFillSize() {
+        tableFillSize++;
+    }
+
+
+    private boolean isFreeSlot(int slotNum) {
+        if (slotNum > 0 && slotNum < tableSize) {
+            return Objects.isNull(symbolTable[slotNum]) ? true : false;
+        }
+        throw new IllegalArgumentException(String.format("Symbol symbolTable index must be between %d and %d!", 0,
+                (tableSize - 1)));
+    }
+
+    private void addToTable(SchemeSymbol symbol, int slotNum) {
+        symbolTable[slotNum] = symbol;
     }
 
 
--- src/test/java/hdm/pk070/jscheme/symbolTable/SchemeSymbolTableTest.java
+++ src/test/java/hdm/pk070/jscheme/symbolTable/SchemeSymbolTableTest.java
@@ -1,9 +1,77 @@
 package hdm.pk070.jscheme.symbolTable;
 
+import hdm.pk070.jscheme.SchemeConstants;
+import hdm.pk070.jscheme.obj.type.SchemeSymbol;
+import hdm.pk070.jscheme.util.ReflectionMethodParam;
+import hdm.pk070.jscheme.util.ReflectionUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 /**
  *
  */
 public class SchemeSymbolTableTest {
 
 
+    private SchemeSymbolTable schemeSymbolTable;
+
+    private SchemeSymbol[] symbolArray;
+
+    @Before
+    public void setUp() {
+        schemeSymbolTable = SchemeSymbolTable.getInstance();
+        symbolArray = (SchemeSymbol[]) ReflectionUtils.getAttributeVal(schemeSymbolTable, "symbolTable");
+    }
+
+    @Test
+    public void testGetOrAdd() {
+        SchemeSymbol testSymbol = schemeSymbolTable.getOrAdd("foobar");
+        SchemeSymbol validationSymbol = schemeSymbolTable.getOrAdd("foobar");
+
+        assertThat("Symbol references must point to same object!", (testSymbol == validationSymbol), equalTo(true));
+    }
+
+    @Test
+    public void testAddToTable() {
+        int randomIndex = getRandomTableIndex();
+        SchemeSymbol testSymbol = SchemeSymbol.createObj("foobar");
+
+        ReflectionUtils.invokeMethod(schemeSymbolTable, "addToTable", new ReflectionMethodParam(SchemeSymbol.class,
+                testSymbol), new ReflectionMethodParam(int.class, randomIndex));
+
+        assertThat("Slot should not be empty!", symbolArray[randomIndex], notNullValue());
+        assertThat("Slot value does not match expected symbol!", symbolArray[randomIndex], equalTo(testSymbol));
+    }
+
+
+    @Test
+    public void testIsFreeSlot() {
+        int randomIndex = getRandomTableIndex();
+
+        assertThat(ReflectionUtils.invokeMethod(schemeSymbolTable, "isFreeSlot", new ReflectionMethodParam(int.class,
+                randomIndex)), equalTo(true));
+        symbolArray[randomIndex] = SchemeSymbol.createObj("foobar");
+        assertThat(ReflectionUtils.invokeMethod(schemeSymbolTable, "isFreeSlot", new ReflectionMethodParam(int.class,
+                randomIndex)), equalTo(false));
+
+    }
+
+    @Test
+    public void testIncrementFillSize() {
+        int tableFillSizeBefore = (int) ReflectionUtils.getAttributeVal(schemeSymbolTable, "tableFillSize");
+        ReflectionUtils.invokeMethod(schemeSymbolTable, "incrementFillSize");
+        int tableFillSizeAfter = (int) ReflectionUtils.getAttributeVal(schemeSymbolTable, "tableFillSize");
+
+        assertThat("Table fill size is not incremented!", tableFillSizeAfter > tableFillSizeBefore, equalTo(true));
+    }
+
+    private int getRandomTableIndex() {
+        return new Random().nextInt((SchemeConstants.INITIAL_SYMBOL_TABLE_SIZE - 1));
+    }
 }
