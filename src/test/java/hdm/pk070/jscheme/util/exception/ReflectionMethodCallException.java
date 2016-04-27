011aaae4e29a7744608c25a9158873c2b89610d5
Patrick Kleindienst <patrick.kleindienst@web.de>
2016-04-27 17:08:20 +0200

    Introduced custom exception for method invocations via reflection

---
+++ src/test/java/hdm/pk070/jscheme/util/exception/ReflectionMethodCallException.java
@@ -0,0 +1,11 @@
+package hdm.pk070.jscheme.util.exception;
+
+/**
+ * Created by patrick on 27.04.16.
+ */
+public class ReflectionMethodCallException extends RuntimeException {
+
+    public ReflectionMethodCallException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
