1e48ad5b63e58995a2928cb900dc886cdd082599
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-04-30 18:40:25 +0200

    Added StringObjReaderTest to suite

--- src/test/java/hdm/pk070/jscheme/SchemeTestSuite.java
+++ src/test/java/hdm/pk070/jscheme/SchemeTestSuite.java
@@ -4,6 +4,7 @@ import hdm.pk070.jscheme.hash.impl.StandardHashAlgProviderTest;
 import hdm.pk070.jscheme.reader.PushbackReaderLearningTest;
 import hdm.pk070.jscheme.reader.SchemeCharacterReaderTest;
 import hdm.pk070.jscheme.reader.SchemeReaderTest;
+import hdm.pk070.jscheme.reader.obj.StringObjReaderTest;
 import hdm.pk070.jscheme.symbolTable.SchemeSymbolTableTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -13,7 +14,8 @@ import org.junit.runners.Suite;
  */
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SchemeReaderTest.class, SchemeCharacterReaderTest.class, PushbackReaderLearningTest.class,
+@Suite.SuiteClasses({SchemeReaderTest.class, StringObjReaderTest.class, SchemeCharacterReaderTest.class,
+        PushbackReaderLearningTest.class,
         StandardHashAlgProviderTest.class, SchemeSymbolTableTest.class})
 public class SchemeTestSuite {
 }
