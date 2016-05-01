ed4fbb40df55c98bec733888cd16132f01c5ea39
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-05-01 19:01:39 +0200

    Implemented SchemeCons object and behavior (e.g. print)

--- src/main/java/hdm/pk070/jscheme/obj/type/SchemeCons.java
+++ src/main/java/hdm/pk070/jscheme/obj/type/SchemeCons.java
@@ -7,12 +7,44 @@ import hdm.pk070.jscheme.obj.SchemeObject;
  */
 public class SchemeCons extends SchemeObject {
 
+    private SchemeObject car;
+    private SchemeObject cdr;
+
+    public SchemeCons(SchemeObject car, SchemeObject cdr) {
+        this.car = car;
+        this.cdr = cdr;
+    }
 
     @Override
     public Object getValue() {
-        System.out.println("Not implemented yet");
-        return null;
+        throw new UnsupportedOperationException("SchemeCons does not have a value, use getCar() and getCdr()");
+    }
+
+    public SchemeObject getCar() {
+        return car;
+    }
+
+    public SchemeObject getCdr() {
+        return cdr;
     }
 
 
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("(");
+        buffer.append(prettyPrintList(this));
+        return buffer.toString();
+    }
+
+    private String prettyPrintList(SchemeObject schemeObject) {
+        String listPrint = "";
+        listPrint += ((SchemeCons) schemeObject).getCar().toString();
+        if (((SchemeCons) schemeObject).getCdr().typeOf(SchemeNil.class)) {
+            listPrint += ")";
+            return listPrint;
+        }
+        listPrint += " ";
+        return listPrint + prettyPrintList(((SchemeCons) schemeObject).getCdr());
+    }
 }
