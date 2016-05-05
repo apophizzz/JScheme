b8044326daa3dc8d63580caed5fc9cc4fd8594f8
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-05-05 20:31:48 +0200

    Added 'roadmap' in the form of comments and TODOs

--- src/main/java/hdm/pk070/jscheme/environment/GlobalEnvironment.java
+++ src/main/java/hdm/pk070/jscheme/environment/GlobalEnvironment.java
@@ -11,7 +11,10 @@ import java.util.Objects;
  */
 public class GlobalEnvironment {
 
+    private static final int INITIAL_GLOBAL_ENV_SIZE = 511;
+
     private static GlobalEnvironment globalEnvironment = null;
+    private static int currentGlobalEnvSize = INITIAL_GLOBAL_ENV_SIZE;
 
     public static GlobalEnvironment getInstance() {
         if (Objects.isNull(globalEnvironment)) {
@@ -25,10 +28,14 @@ public class GlobalEnvironment {
     }
 
     public SchemeObject get(SchemeSymbol expression) {
+        // TODO: implementation
         return null;
     }
 
     public void put(SchemeSymbol envKey, SchemeObject envValue) {
+        // 1. Create numeric key based on envKey (maybe hashcode?)
+        // 2. Compute startIndex = key (step 1) % current size of global env
+        // 3. Before searching the next free slot, set nextIndex = startIndex (see step 2)
         return;
     }
 }
