4ed7a208ee0ad81a1820d98e137ec44153dc7f47
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-05-05 18:07:03 +0200

    Added SymbolEvaluator and test class for evaluating the SchemeSymbols returned by SchemeReader

---
+++ src/main/java/hdm/pk070/jscheme/eval/SymbolEvaluator.java
@@ -0,0 +1,42 @@
+package hdm.pk070.jscheme.eval;
+
+import hdm.pk070.jscheme.environment.GlobalEnvironment;
+import hdm.pk070.jscheme.error.SchemeError;
+import hdm.pk070.jscheme.obj.SchemeObject;
+import hdm.pk070.jscheme.obj.type.SchemeSymbol;
+import hdm.pk070.jscheme.reader.SchemeReader;
+
+import java.util.Objects;
+
+/**
+ * This evaluator is called in case the {@link SchemeReader} returned a {@link SchemeSymbol}
+ * as an intermediate result. The {@link SymbolEvaluator}'s job is to lookup the symbol in
+ * {@link GlobalEnvironment} and return it or throw an error in case it does not exist.
+ *
+ * @author patrick.kleindienst
+ */
+class SymbolEvaluator extends AbstractEvaluator {
+
+    static SymbolEvaluator getInstance() {
+        return new SymbolEvaluator();
+    }
+
+    private SymbolEvaluator() {
+    }
+
+    @Override
+    public SchemeObject doEval(SchemeSymbol expression) throws SchemeError {
+        // 1. get symbol from global environment
+        LOGGER.debug(String.format("Evaluating symbol %s ...", expression.getValue()));
+        SchemeObject value = GLOBAL_ENVIRONMENT.get(expression);
+        // 2. throw SchemeError if not present in environment
+        if (Objects.isNull(value)) {
+            LOGGER.debug(String.format("Unable to find symbol %s in global environment. Throw SchemeError.",
+                    expression.getValue()));
+            throw new SchemeError(String.format("undefined variable %s", expression.getValue()));
+        }
+        // 3. return existing value
+        LOGGER.debug(String.format("Evaluation of symbol %s yielded: " + value.getValue(), expression.getValue()));
+        return value;
+    }
+}
---
+++ src/test/java/hdm/pk070/jscheme/eval/SymbolEvaluatorTest.java
@@ -0,0 +1,64 @@
+package hdm.pk070.jscheme.eval;
+
+import hdm.pk070.jscheme.environment.GlobalEnvironment;
+import hdm.pk070.jscheme.error.SchemeError;
+import hdm.pk070.jscheme.obj.SchemeObject;
+import hdm.pk070.jscheme.obj.type.SchemeInteger;
+import hdm.pk070.jscheme.obj.type.SchemeSymbol;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * A JUnit4 test class for testing {@link SymbolEvaluator}.
+ *
+ * @author patrick.kleindienst
+ */
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.management.*")
+@PrepareForTest(GlobalEnvironment.class)
+public class SymbolEvaluatorTest {
+
+    private SymbolEvaluator symbolEvaluator;
+    private GlobalEnvironment globalEnvironmentMock;
+
+    @Before
+    public void setUp() {
+        globalEnvironmentMock = Mockito.mock(GlobalEnvironment.class);
+        Mockito.when(globalEnvironmentMock.get(new SchemeSymbol("foo"))).thenReturn(new SchemeInteger(42));
+        Mockito.when(globalEnvironmentMock.get(new SchemeSymbol("bar"))).thenReturn(null);
+
+        PowerMockito.mockStatic(GlobalEnvironment.class);
+        PowerMockito.when(GlobalEnvironment.getInstance()).thenReturn(globalEnvironmentMock);
+
+        symbolEvaluator = SymbolEvaluator.getInstance();
+    }
+
+
+    @Test
+    public void testEvalDefinedSymbol() throws SchemeError {
+        SchemeObject fooResult = symbolEvaluator.doEval(new SchemeSymbol("foo"));
+
+        assertThat("fooResult must not be null!", fooResult, notNullValue());
+        assertThat(String.format("fooResult does not match expected type %s!", SchemeInteger.class.getSimpleName()),
+                fooResult.typeOf(SchemeInteger.class), equalTo(true));
+        assertThat(String.format("fooResult does not match expected value %d!", 42), fooResult, equalTo(new
+                SchemeInteger(42)));
+    }
+
+
+    @Test(expected = SchemeError.class)
+    public void testEvalUndefinedSymbol() throws SchemeError {
+        symbolEvaluator.doEval(new SchemeSymbol("bar"));
+    }
+}
