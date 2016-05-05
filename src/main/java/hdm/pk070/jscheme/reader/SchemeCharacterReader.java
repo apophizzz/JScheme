86f63fcf79e7035cefd4b51d441215e4dec93989
Patrick Kleindienst <patrick.kleindienst1990@gmail.com>
2016-05-05 13:10:19 +0200

    Fixed broken imports causing build failure

--- src/main/java/hdm/pk070/jscheme/reader/SchemeCharacterReader.java
+++ src/main/java/hdm/pk070/jscheme/reader/SchemeCharacterReader.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
