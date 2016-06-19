6b39c92961aa7fa828ec72bc3486db38de69ad1c
patrick.kleindienst <patrick.kleindienst@web.de>
2016-06-19 12:26:41 +0200

    Introduced interface as an indirection

---
+++ src/main/java/hdm/pk070/jscheme/table/environment/Environment.java
@@ -0,0 +1,18 @@
+package hdm.pk070.jscheme.table.environment;
+
+import hdm.pk070.jscheme.error.SchemeError;
+
+import java.util.Optional;
+
+/**
+ * Created by patrick on 17.06.16.
+ */
+public interface Environment<KEY, VALUE> {
+
+    boolean isGlobal();
+
+    Optional<VALUE> get(KEY key);
+
+    VALUE add(VALUE value) throws SchemeError;
+
+}
