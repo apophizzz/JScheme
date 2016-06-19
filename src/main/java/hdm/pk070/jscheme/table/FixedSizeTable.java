3fdb41c0b324fd861340b248869ef38e24c8b9b2
patrick.kleindienst <patrick.kleindienst@web.de>
2016-06-19 12:29:01 +0200

    Implemented table with fixed size

--- src/main/java/hdm/pk070/jscheme/table/FixedSizeTable.java
+++ src/main/java/hdm/pk070/jscheme/table/FixedSizeTable.java
@@ -1,10 +1,49 @@
 package hdm.pk070.jscheme.table;
 
-import hdm.pk070.jscheme.obj.SchemeObject;
+import hdm.pk070.jscheme.error.SchemeError;
+import hdm.pk070.jscheme.table.environment.entry.EnvironmentEntry;
+
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Created by patrick on 16.05.16.
  */
-public abstract class FixedSizeTable<KEY, VALUE extends SchemeObject> extends BaseTable<KEY, VALUE> {
+public abstract class FixedSizeTable<KEY, VALUE> extends BaseTable<KEY, VALUE> {
+
+    private Object[] entries;
+
+    protected FixedSizeTable(int fixedSize) {
+        this.entries = new Object[fixedSize];
+    }
+
+    public Optional<VALUE> get(KEY key) {
+        Objects.requireNonNull(key);
+
+        // Iteration über alle Slots
+        for (Object entry : entries) {
+            // extrahiere und vergleiche Key
+            if (Objects.nonNull(entry) && keysMatch(key, (VALUE) entry)) {
+                return Optional.of((VALUE) entry);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public VALUE add(VALUE value) throws SchemeError {
+        for (int i = 0; i < entries.length; i++) {
+            if (Objects.nonNull(entries[i])) {
+                if ((((EnvironmentEntry) value).getKey() == ((EnvironmentEntry)
+                        entries[i]).getKey())) {
+                    entries[i] = value;
+                    return value;
+                }
+            } else {
+                entries[i] = value;
+                return value;
+            }
+        }
+        throw new SchemeError("FixedSizeTable overflow!");
+    }
 
 }
