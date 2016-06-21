f7a1ec5df273559bf41d15df7e41627ea4dc5c5f
patrick.kleindienst <patrick.kleindienst@web.de>
2016-06-21 17:27:14 +0200

    Added test classes for built-in + and - to suite

--- src/test/java/hdm/pk070/jscheme/obj/SchemeObjectTestSuite.java
+++ src/test/java/hdm/pk070/jscheme/obj/SchemeObjectTestSuite.java
@@ -1,6 +1,9 @@
 package hdm.pk070.jscheme.obj;
 
-import hdm.pk070.jscheme.obj.type.simple.SchemeConsTest;
+import hdm.pk070.jscheme.obj.function.builtin.SchemeBuiltinMinusTest;
+import hdm.pk070.jscheme.obj.function.builtin.SchemeBuiltinPlusTest;
+import hdm.pk070.jscheme.obj.simple.SchemeObjectTest;
+import hdm.pk070.jscheme.obj.simple.SchemeConsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -9,6 +12,7 @@ import org.junit.runners.Suite;
  */
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SchemeObjectTest.class, SchemeConsTest.class})
+@Suite.SuiteClasses({SchemeObjectTest.class, SchemeConsTest.class, SchemeBuiltinPlusTest.class,
+        SchemeBuiltinMinusTest.class})
 public class SchemeObjectTestSuite {
 }
