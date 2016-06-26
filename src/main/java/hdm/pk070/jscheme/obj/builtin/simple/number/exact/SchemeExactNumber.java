b7a9078490c8507d2d3d03e4b67105d0400e6c49
patrick.kleindienst <patrick.kleindienst@web.de>
2016-06-26 16:19:13 +0200

    Added exact numbers as base type for integers

---
+++ src/main/java/hdm/pk070/jscheme/obj/builtin/simple/number/exact/SchemeExactNumber.java
@@ -0,0 +1,9 @@
+package hdm.pk070.jscheme.obj.builtin.simple.number.exact;
+
+import hdm.pk070.jscheme.obj.builtin.simple.number.SchemeNumber;
+
+/**
+ * Created by patrick on 24.06.16.
+ */
+public abstract class SchemeExactNumber extends SchemeNumber {
+}
