142e00dc0d8d859cea01182c746d6f8c0eeeb96a
patrick.kleindienst <patrick.kleindienst@web.de>
2016-07-05 16:00:29 +0200

    Removed unused imports

--- src/test/java/hdm/pk070/jscheme/obj/builtin/SchemeBuiltinTestSuite.java
+++ src/test/java/hdm/pk070/jscheme/obj/builtin/SchemeBuiltinTestSuite.java
@@ -1,13 +1,7 @@
 package hdm.pk070.jscheme.obj.builtin;
 
-import hdm.pk070.jscheme.obj.builtin.function.*;
+import hdm.pk070.jscheme.obj.builtin.function.SchemeBuiltinFunctionTestSuite;
 import hdm.pk070.jscheme.obj.builtin.simple.SchemeBuiltinSimpleTypesTestSuite;
-import hdm.pk070.jscheme.obj.builtin.simple.SchemeConsTest;
-import hdm.pk070.jscheme.obj.builtin.simple.SchemeObjectTest;
-import hdm.pk070.jscheme.obj.builtin.simple.number.exact.SchemeFractionTest;
-import hdm.pk070.jscheme.obj.builtin.simple.number.exact.SchemeIntegerTest;
-import hdm.pk070.jscheme.obj.builtin.simple.number.floatComplex.SchemeFloat;
-import hdm.pk070.jscheme.obj.builtin.simple.number.floatComplex.SchemeFloatTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
