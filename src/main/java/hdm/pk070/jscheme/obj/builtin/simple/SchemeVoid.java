265cff21e5d5e7516356fbd3da050af5da753162
patrick.kleindienst <patrick.kleindienst@web.de>
2016-07-06 15:36:45 +0200

    Introduced a void equivalent for JScheme (required by built-in syntax)

---
+++ src/main/java/hdm/pk070/jscheme/obj/builtin/simple/SchemeVoid.java
@@ -0,0 +1,16 @@
+package hdm.pk070.jscheme.obj.builtin.simple;
+
+import hdm.pk070.jscheme.obj.SchemeObject;
+
+/**
+ * @author patrick.kleindienst
+ */
+public class SchemeVoid extends SchemeObject {
+
+
+    @Override
+    public Object getValue() {
+        //        throw new UnsupportedOperationException();
+        return "";
+    }
+}
