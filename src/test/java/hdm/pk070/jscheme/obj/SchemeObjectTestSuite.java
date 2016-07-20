44a50173f52aa37af84895cbb8746b99419f5ce9
patrick.kleindienst <patrick.kleindienst@web.de>
2016-07-20 19:20:39 +0200

    Added test cases concerning user-defined functions to test suite

--- src/test/java/hdm/pk070/jscheme/obj/SchemeObjectTestSuite.java
+++ src/test/java/hdm/pk070/jscheme/obj/SchemeObjectTestSuite.java
@@ -1,6 +1,7 @@
 package hdm.pk070.jscheme.obj;
 
 import hdm.pk070.jscheme.obj.builtin.SchemeBuiltinTestSuite;
+import hdm.pk070.jscheme.obj.custom.SchemeCustomUserFunctionTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -9,6 +10,6 @@ import org.junit.runners.Suite;
  */
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({SchemeBuiltinTestSuite.class})
+@Suite.SuiteClasses({SchemeBuiltinTestSuite.class, SchemeCustomUserFunctionTest.class})
 public class SchemeObjectTestSuite {
 }
