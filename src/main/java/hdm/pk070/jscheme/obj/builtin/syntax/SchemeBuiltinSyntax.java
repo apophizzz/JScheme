c44916890112df9c4b46e6a9634cfc7c1b86f9ed
patrick.kleindienst <patrick.kleindienst@web.de>
2016-08-16 12:11:47 +0200

    Added some documentation.

--- src/main/java/hdm/pk070/jscheme/obj/builtin/syntax/SchemeBuiltinSyntax.java
+++ src/main/java/hdm/pk070/jscheme/obj/builtin/syntax/SchemeBuiltinSyntax.java
@@ -7,6 +7,8 @@ import hdm.pk070.jscheme.table.environment.Environment;
 import hdm.pk070.jscheme.table.environment.entry.EnvironmentEntry;
 
 /**
+ * A base class for every syntax feature coming with JScheme.
+ *
  * @author patrick.kleindienst
  */
 public abstract class SchemeBuiltinSyntax extends SchemeObject {
@@ -17,9 +19,26 @@ public abstract class SchemeBuiltinSyntax extends SchemeObject {
         this.internalName = internalName;
     }
 
+    /**
+     * Applying the built-in syntax to the given parameters.
+     *
+     * @param argumentList
+     *         A list of arguments the syntax shall be applied to.
+     * @param environment
+     *         The environment within which the arguments shall be evaluated.
+     * @return The result of applying a certain syntax on a given argument list.
+     * @throws SchemeError
+     *         If syntax is not invoked as expected.
+     */
     public abstract SchemeObject apply(SchemeObject argumentList, Environment<SchemeSymbol, EnvironmentEntry>
             environment) throws SchemeError;
 
+    /**
+     * We assume that a built-in syntax does not have a value to return.
+     * The current solution might not be the best one.
+     *
+     * @return Nothing, always throws {@link UnsupportedOperationException} instead.
+     */
     @Override
     public Object getValue() {
         throw new UnsupportedOperationException();
