c432df37f2ebc47ffd2d559da32844a094675920
patrick.kleindienst <patrick.kleindienst@web.de>
2016-08-20 13:14:22 +0200

    Fixed imports.

--- src/main/java/hdm/pk070/jscheme/cp/trampoline/SchemeTrampoline.java
+++ src/main/java/hdm/pk070/jscheme/cp/trampoline/SchemeTrampoline.java
@@ -1,6 +1,5 @@
 package hdm.pk070.jscheme.cp.trampoline;
 
-import hdm.pk070.jscheme.cp.JSchemeCP;
 import hdm.pk070.jscheme.error.SchemeError;
 import hdm.pk070.jscheme.obj.SchemeContinuationFunction;
 import hdm.pk070.jscheme.obj.continuation.SchemeContinuation;
